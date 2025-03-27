13cd702730eab0832de862ee10656e1d1e645171
Valtteri <valtteri.putaansuu@gmail.com>
2025-03-27 17:25:58 +0200

    Auth logiikka ok, env ja muita tiedosto ongelmia vielä

--- scenes/firebase.gd
+++ scenes/firebase.gd
@@ -3,27 +3,27 @@ extends Control
 #main method
 func _ready():
 	Firebase.Auth.login_succeeded.connect(_on_firebaseAuth_login_succeeded)
-	Firebase.Auth.signup_succeeded.connect(_on_firebaseAuth_login_succeeded)
+	Firebase.Auth.signup_succeeded.connect(_on_firebaseAuth_signup_succeeded)
 	Firebase.Auth.login_failed.connect(on_login_failed)
 	Firebase.Auth.signup_failed.connect(on_signup_failed)
 	
 	if Firebase.Auth.check_auth_file():
-		$StateLabel.text = "Logged in"
+		%StateLabel.text = "Logged in"
 		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 #register with email
 func _on_signup_withEmail_button_pressed():
-	var email = $EmailLineEdit.text
-	var password = $PasswordLineEdit.text
+	var email = %EmailLineEdit.text
+	var password = %PasswordLineEdit.text
 	Firebase.Auth.signup_with_email_and_password(email, password)
-	$StateLabel.text = "Sign up"
+	%StateLabel.text = "Sign up"
 
 #login with email
 func _on_login_withEmail_button_pressed():
-	var email = $EmailLineEdit.text
-	var password = $PasswordLineEdit.text
+	var email = %EmailLineEdit.text
+	var password = %PasswordLineEdit.text
 	Firebase.Auth.login_with_email_and_password(email, password)
-	$StateLabel.text = "Login"
+	%StateLabel.text = "Login"
 
 #login anonymously
 func _on_login_anonymous_button_pressed():
@@ -32,7 +32,7 @@ func _on_login_anonymous_button_pressed():
 #when login succesfull
 func _on_firebaseAuth_login_succeeded(auth):
 	print(auth)
-	$StateLabel.text = "Login succesfull!"
+	%StateLabel.text = "Login succesfull!"
 	Firebase.Auth.save_auth(auth)
 	Firebase.Auth.load_auth()
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
@@ -40,19 +40,19 @@ func _on_firebaseAuth_login_succeeded(auth):
 #when signup succesfull
 func _on_firebaseAuth_signup_succeeded(auth):
 	print(auth)
-	$StateLabel.text = "Sign up succesfull!"
+	%StateLabel.text = "Sign up succesfull!"
 	Firebase.Auth.save_auth(auth)
-	Firebase.AUth.load_auth
+	Firebase.Auth.load_auth()
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 #when login unsuccesfull
 func on_login_failed(error_code, message):
 	print("error:" + str(error_code))
 	print("message:" + str(message))
-	$StateLabel.text = "Login failed"
+	%StateLabel.text = "Login failed"
 
 #when signup unsuccesfull
 func on_signup_failed(error_code, message):
 	print("error:" + str(error_code))
 	print("message:" + str(message))
-	$StateLabel.text = "Sign up failed"
+	%StateLabel.text = "Sign up failed"

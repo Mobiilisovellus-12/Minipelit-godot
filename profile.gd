0c8d360ce0f9452f107031788933aa4c4adbf2e9
mikkohir <155948800+mikkohir@users.noreply.github.com>
2025-03-27 13:41:40 +0200

    Profile-scene lisätty

---
+++ profile.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _ready():
+	get_viewport().size = DisplayServer.screen_get_size()
+	
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_packed(load("res://scenes/main_menu.tscn"))

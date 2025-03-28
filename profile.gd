3b129e579d6697be0b007f8d41db5f51b99db73c
mikkohir <155948800+mikkohir@users.noreply.github.com>
2025-03-28 14:04:37 +0200

    Uloskirjautuminen lisätty, ja toimii

--- profile.gd
+++ profile.gd
@@ -3,5 +3,18 @@ extends Control
 func _ready():
 	get_viewport().size = DisplayServer.screen_get_size()
 	
+	
+	if Firebase.Auth.check_auth_file():
+		%Email.text = "Logged in"
+		%LogOutButton.visible = true
+			
+	
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_packed(load("res://scenes/main_menu.tscn"))
+
+
+func _on_log_out_button_pressed() -> void:
+	Firebase.Auth.logout()
+	%Username.text = "Guest"
+	%Email.text = ""
+	%LogOutButton.visible = false

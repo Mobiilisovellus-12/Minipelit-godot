8a100f0cc94096c829362e53483cc19b07510336
Valtteri <valtteri.putaansuu@gmail.com>
2025-04-07 17:54:37 +0300

    gameselect 1, shopscreen 1/2

--- scenes/game_select.gd
+++ scenes/game_select.gd
@@ -22,6 +22,8 @@ func _add_ui_theme():
 func _on_button_pressed():
 	get_tree().change_scene_to_packed(load("res://scenes/main_menu.tscn"))
 
+func _to_shop():
+	get_tree().change_scene_to_packed(load("res://scenes/shop-screen.tscn"))
 
 #function redirects to desired scene
 func _on_play_button_pressed(path):
@@ -42,12 +44,13 @@ func generate_ui():
 			game_label.text = game_entry.get("gameName")
 			game_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 			game_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+			game_label.add_theme_font_size_override("font_size", 50)
 			
 			#creates the games img
 			var game_texture = TextureRect.new()
 			game_texture.texture = load(game_entry.get("gameImg"))
-			game_texture.expand_mode = TextureRect.EXPAND_IGNORE_SIZE #Vitun vitun
-			game_texture.custom_minimum_size = Vector2(50, 500) #vitun vittu...
+			#game_texture.expand_mode = TextureRect.STRETCH_SCALE
+			#game_texture.size = Vector2(100, 100)
 			
 			#creates the path to the pre-game scene
 			var scene_path = game_entry.get("scenePath")
@@ -55,14 +58,22 @@ func generate_ui():
 			#creates the play button (redirects to pre-game scene)
 			var play_button = Button.new()
 			play_button.text = "PLAY"
+			play_button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+			play_button.custom_minimum_size = Vector2(0, 50)
 			play_button.theme = button_theme
+			play_button.add_theme_font_size_override("font_size", 50)
 			play_button.pressed.connect(_on_play_button_pressed.bind(scene_path))
 			
 			var spacer = Control.new()
 			spacer.custom_minimum_size = Vector2(0, 100)
 			
 			#adds the ui components (label, texture, button) to the VBoxContainer
-			scontainer.add_child(game_label)
-			scontainer.add_child(game_texture)
-			scontainer.add_child(play_button)
+			var entry_container = VBoxContainer.new()
+			#entry_container.alignment = BoxContainer.ALIGNMENT_CENTER
+			#entry_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+			entry_container.add_child(game_label)
+			entry_container.add_child(game_texture)
+			entry_container.add_child(play_button)
+			
+			scontainer.add_child(entry_container)
 			scontainer.add_child(spacer)
--- scenes/shop_screen.gd
+++ scenes/shop_screen.gd
@@ -1,8 +1,42 @@
 extends Control
 
+var shopData = {}
+var jsonFilePath = "res://resources/shop.json"
+
+const jsonUtils = preload("res://scripts/read_json.gd")
+const button_theme = preload("res://themes/button_theme.tres")
+const back_button_theme = preload("res://themes/back_button_theme.tres")
+
 func _ready():
 	# Setting the scene size to screen size
 	get_viewport().size = DisplayServer.screen_get_size()
+	shopData = jsonUtils.read_json(jsonFilePath)
+	generate_ui()
 	
 func _on_button_pressed():
 	get_tree().change_scene_to_packed(load("res://scenes/main_menu.tscn"))
+	
+func generate_ui():
+	var scontainer = $ColorRect/ScrollContainer/VBoxContainer
+	
+	for item in shopData.keys():
+		for i in range (shopData[item].size()):
+			var shop_entry = shopData[item][i]
+			
+			var shop_label = Label.new()
+			shop_label.text = shop_entry.get("itemName")
+			
+			var shop_price = Label.new()
+			shop_label.text = shop_entry.get("itemPrice")
+			
+			var shop_pay_button = Button.new()
+			shop_pay_button.text = "Pay"
+			shop_pay_button.theme = button_theme
+			
+			var spacer = Control.new()
+			spacer.size = Vector2(0, 100)
+			
+			scontainer.add_child(shop_label)
+			scontainer.add_child(shop_price)
+			scontainer.add_child(shop_pay_button)
+			scontainer.add_child(spacer)

cd69614259546b69caec13adbe7b88591e92494b
EliasHir <155960647+EliasHir@users.noreply.github.com>
2025-04-21 22:15:15 +0300

    New additions/fixes
    improved shooting, bug fixes

--- scenes/disc_shooting.gd
+++ scenes/disc_shooting.gd
@@ -7,3 +7,5 @@ func _ready():
 	get_viewport().size = DisplayServer.screen_get_size()
 	crosshair.position.x = get_viewport().size.x / 2 - 64
 	crosshair.position.y = get_viewport().size.y / 2 - 64
+	
+	$joystick_ui/CanvasLayer/TextureButton.shoot_pressed.connect($DiscShootingPlayer.shoot)
